05fbd8fdf1ad79a4950b8450d3aa420d61dbdaa5
Kareem moore <moorekareem877@gmail.com>
2024-11-21 10:44:34 -0500

    Update

--- Chang.sql
+++ Chang.sql
@@ -23,7 +23,7 @@ CREATE TABLE Students (
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
     stock TINYINT(10)
-);
+);  
 
 INSERT INTO products (name, description, price, stock)
 VALUES 
@@ -32,3 +32,38 @@ VALUES
 ('Headphones', 'Noise-canceling headphones', 150.00, 10),
 ('Tablet', 'A lightweight tablet', 500.00, 10),
 ('Monitor', '4K Ultra HD monitor', 300.00, 5);
+
+  CREATE TABLE my_Events (
+  event_name  VARCHAR(100),
+  event_date TIMESTAMP,
+  event_time Time,
+  event_dataTime DATETIME,
+  event_timestamp TIMESTAMP
+  
+  );
+  
+  INSERT INTO my_events
+(`event_name`,
+`event_date`,
+`event_time`,
+`event_dateTime`,
+`event_timeStamp`)
+VALUES
+('Tech Conference AfroTech', '2024-11-13', '09:00:00', '2024-11-13 09:00:00', '09:00:00'),
+('Tech Conference Launchpad Hiring', '2024-11-13', '09:00:00', '2024-11-13 09:00:00', '09:00:00');
+
+select * from my_events 
+-- Where event_date BETWEEN current_date() AND current_date() + 7;
+WHERE event_date BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY) + INTERVAL 1 SECOND;
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name_char CHAR(10),
+    name_varchar VARCHAR(10)
+);
+
+INSERT INTO Users (name_char, name_varchar)
+VALUES 
+    ('Alice', 'Alice'),
+    ('Bob', 'Bob'),
+    ('Jonathan', 'Jonathan'); 
